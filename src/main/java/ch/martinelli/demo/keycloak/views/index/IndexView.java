8641258d57a0e3b5b3ca57c1f3c8a1efde917dba
Michael Quaschny <m.quaschny@dbiuss.de>
2024-05-06 07:55:40 +0200

    Changed PageTitle

--- src/main/java/ch/martinelli/demo/keycloak/views/index/IndexView.java
+++ src/main/java/ch/martinelli/demo/keycloak/views/index/IndexView.java
@@ -1,21 +1,63 @@
 package ch.martinelli.demo.keycloak.views.index;
 
 import ch.martinelli.demo.keycloak.views.MainLayout;
+import com.vaadin.flow.component.Html;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
-@PageTitle("Index")
+@PageTitle("Welcome")
 @Route(value = "", layout = MainLayout.class)
 @AnonymousAllowed()
-public class IndexView extends HorizontalLayout {
+public class IndexView extends VerticalLayout {
 
     public IndexView() {
         setPadding(true);
 
-        add(new H1("Index"));
+        add(new H2("Hallo und willkommen"));
+
+        String yourContent_notloggedin ="Auf dieser Seite werden Tools und Hilfsmittel bereitgestellt, um FVM-Tätigkeiten zu vereinfachen.<br />" +
+                "Ebenso wird den Justizen ermöglicht, Informationen direkt aus der <b>eKP</b> bzw. <b>EGVP-E</b> zu entnehmen.<br />" +
+                "Die sich aktuell noch in Planung befindlichen Funktionen sind mit <i>geplant</i> gekennzeichnet.<br />" +
+                "Ideen, Anregungen oder Verbesserungsvorschläge sind herzlich willkommen!&#128512;<br /><br />" +
+                "Bitte als erstes einloggen!<br /><br />" +
+                "Viele Grüße<br /><b>Euer Dataport FVM-Team</b>" ;
+
+        String yourContent_loggedin ="Auf dieser Seite werden Tools und Hilfsmittel bereitgestellt, um FVM-Tätigkeiten zu vereinfachen.<br />" +
+                "Ebenso wird den Justizen ermöglicht, Informationen direkt aus der <b>eKP</b> bzw. <b>EGVP-E</b> zu entnehmen.<br />" +
+                "Die sich aktuell noch in Planung befindlichen Funktionen sind mit <i>geplant</i> gekennzeichnet.<br />" +
+                "Ideen, Anregungen oder Verbesserungsvorschläge sind herzlich willkommen!&#128512;<br /><br />" +
+                "Bitte im linken Auswahlmenü die gewünschte Funktionalität auswählen.<br /><br />" +
+                "Viele Grüße<br /><b>Euer Dataport FVM-Team</b>" ;
+
+        Html html_notLoggedin = new Html("<text>" + yourContent_notloggedin + "</text>");
+        Html html_Loggedin = new Html("<text>" + yourContent_loggedin + "</text>");
+
+        add(html_notLoggedin,html_Loggedin);
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentUserName = authentication.getName();
+
+        if (currentUserName=="anonymousUser")
+        {
+            html_Loggedin.setVisible(false);
+            html_notLoggedin.setVisible(true);
+
+
+        }
+        else
+        {
+            html_Loggedin.setVisible(true);
+            html_notLoggedin.setVisible(false);
+
+
+        }
     }
 
 }
